572da1850d49d3e909f932366afdea2a457d6f40
mine is: Ashiqur rahman <ashiqurcseru@gmail.com>
2017-04-08 19:53:21 +0600

    Extract printBanner() method

--- Refactorable.java
+++ Refactorable.java
@@ -9,9 +9,7 @@ public class Refactorable {
 		double outstanding = 0.0;
 		
 		// print banner
-		System.out.println("*************************");
-		System.out.println("***** Customer Owes *****");
-		System.out.println("*************************");
+		printBanner();
 		
 		// calculate outstanding
 		while(e.hasMoreElements()){
@@ -22,6 +20,12 @@ public class Refactorable {
 		// print details
 		System.out.println("Name : " + _name);
 		System.out.println("Amount : " + outstanding);
+	}
+
+	public void printBanner() {
+		System.out.println("*************************");
+		System.out.println("***** Customer Owes *****");
+		System.out.println("*************************");
 	}
 
 }
